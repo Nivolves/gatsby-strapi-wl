a96d6ac38a7c11b0e92f2ae183e133197bba781a
Nivolves2000 <48019409+Nivolves2000@users.noreply.github.com>
2019-10-05 22:09:01 +0300

    feat(strapi): gatsby-source-strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-'use strict'
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
@@ -7,55 +7,59 @@ module.exports = {
     siteUrl: 'https://nodeart.io',
     author: {
       name: '@Nodeart',
-    }
+    },
   },
   plugins: [
     {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'https://content.nodeart.app',
+        queryLimit: 1000,
+        contentTypes: ['languages', 'settings', 'defaultpages'],
+        loginData: {
+          identifier: process.env.LOGIN,
+          password: process.env.PASSWORD,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+    },
+    {
       resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
-        'fonts': [
+        fonts: [
           {
-            'family': 'Roboto',
-            'variants': [
-              '400',
-              '900',
-            ],
-            'subsets': [
-              'cyrillic',
-              'latin'
-            ]
-          }
+            family: 'Roboto',
+            variants: ['400', '900'],
+            subsets: ['cyrillic', 'latin'],
+          },
         ],
-        'formats': [
-          'woff',
-          'woff2',
-          'ttf',
-          'eot'
-        ]
-      }
+        formats: ['woff', 'woff2', 'ttf', 'eot'],
+      },
     },
     {
       resolve: 'gatsby-plugin-layout',
       options: {
-        component: require.resolve( './src/components/layout.tsx' )
-      }
+        component: require.resolve('./src/components/layout.tsx'),
+      },
     },
     {
       resolve: 'gatsby-plugin-eslint',
       options: {
         test: /\.ts$|\.tsx$/,
         exclude: /(node_modules|.cache|public|)/,
-        stages: [ 'develop' ],
+        stages: ['develop'],
         options: {
           emitWarning: true,
-          failOnError: true
-        }
-      }
+          failOnError: true,
+        },
+      },
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-typescript',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet'
-  ]
-}
+    'gatsby-plugin-react-helmet',
+  ],
+};
